7b2c4ac0bd617cad5100dd8785f94cfdd603987b
AplayER <673916180@qq.com>
2016-03-31 20:55:00 +0800

    PrintBooleanArray

---
+++ java/src/com/asimes/algorithm/basic/IntegerToBinaryString.java
@@ -0,0 +1,24 @@
+package com.asimes.algorithm.basic;
+
+/**
+ * Created by lianglab on 2016/3/31.
+ * ¹¦ÄÜ £º
+ */
+public class IntegerToBinaryString{
+    public static void main(String[] args) {
+        simpleSolution(5);
+        ownSolution(5);
+    }
+
+    public static void simpleSolution(int arg){
+        System.out.println(Integer.toBinaryString(arg));
+    }
+
+    public static void ownSolution(int arg){
+        String s = "";
+        for (int i = arg; i > 0; i/=2) {
+            s = (i%2) +s;
+        }
+        System.out.println(s);
+    }
+}
