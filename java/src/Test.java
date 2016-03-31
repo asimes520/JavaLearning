957a2a3dd6fb42b1054b9d019bb07dd865c080ad
asimes <asimes@163.com>
2016-03-31 16:41:59 +0800

    test

--- java/src/Test.java
+++ java/src/Test.java
@@ -1,9 +1,8 @@
-/**
- * Created by lianglab on 2016/3/31.
- * ¹¦ÄÜ £º
- */
+package src;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(1);
     }
 }
